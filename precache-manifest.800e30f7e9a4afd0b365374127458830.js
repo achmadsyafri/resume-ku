23e2a6b5eca1827884ac8f7e320d96f8c92ab48b
achmadsyafri <achmadsyafri11@gmail.com>
2020-10-02 18:57:24 +0800

    Updates

---
+++ precache-manifest.800e30f7e9a4afd0b365374127458830.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f717804a3b1621a78851aac03abbc388",
+    "url": "/resume-ku/index.html"
+  },
+  {
+    "revision": "d1dc910009fe8fcd029d",
+    "url": "/resume-ku/static/css/main.2bbc0dc9.chunk.css"
+  },
+  {
+    "revision": "3843250ab2fc05bfe363",
+    "url": "/resume-ku/static/js/2.7f2ba78b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/resume-ku/static/js/2.7f2ba78b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1dc910009fe8fcd029d",
+    "url": "/resume-ku/static/js/main.10ee57f3.chunk.js"
+  },
+  {
+    "revision": "7171884fb1ba626c2865",
+    "url": "/resume-ku/static/js/runtime-main.88dd605f.js"
+  },
+  {
+    "revision": "7802d8b27fcb19893ce6b38c0789268e",
+    "url": "/resume-ku/static/media/Raleway-Bold.7802d8b2.ttf"
+  },
+  {
+    "revision": "6c084270ccdeb72fd9f5a5144cea628f",
+    "url": "/resume-ku/static/media/Raleway-Light.6c084270.ttf"
+  },
+  {
+    "revision": "1b98b6fd8c6f87a268c5220126a36fcc",
+    "url": "/resume-ku/static/media/me2.1b98b6fd.png"
+  }
+]);
